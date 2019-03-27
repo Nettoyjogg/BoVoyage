9ae971fe6c2f443d7e9afff743e9ae9400456bd3
IN-BR-010 <IN-BR-010@IN-BR-010>
2019-03-27 09:27:19 +0100

    jogg: modele 3

---
+++ BoVoyage/src/main/java/fr/adaming/model/AgenceVoyageExte.java
@@ -0,0 +1,12 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+public class AgenceVoyageExte implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	//déclaration des attributs
+	private String nom;
+
+}
